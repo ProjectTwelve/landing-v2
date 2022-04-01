bcfc11dfe611a0dd9ccefa1674f4235e4d3e6997
秦书羽 <qinshuyu@bytedance.com>
2022-04-01 21:45:41 +0800

    完善结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { PageHome } from "./pages/home/PageHome";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    overflow: auto;
+`;
 
 function App() {
     return (
-        <div className="app">
+        <StyledApp>
             <PageHome />
-        </div>
+        </StyledApp>
     );
 }
 
