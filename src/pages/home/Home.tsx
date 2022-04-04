3e9c1efe05f95d58c7893af50ca4e3e8e1a96275
秦书羽 <qinshuyu@bytedance.com>
2022-04-04 17:19:19 +0800

    完善

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,16 +5,6 @@ import './Home.less';
 export const Home: React.FC = () => {
     return (
         <div className='home'>
-            <canvas
-                id='canvas'
-                style={{
-                    position: 'absolute',
-                    left: 0,
-                    top: 0,
-                    width: '100%',
-                    height: '100%',
-                }}
-            ></canvas>
             <HomeGL />
         </div>
     );
