25393e18d638bd58ad30a9497260f6e0117e39a2
秦书羽 <qinshuyu@bytedance.com>
2022-04-04 10:51:54 +0800

    完善 less

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,10 @@
 /* config-overrides.js */
-const { override } = require('customize-cra');
+const path = require('path');
+const { override, addWebpackAlias } = require('customize-cra');
 const addLessLoader = require('customize-cra-less-loader');
-module.exports = override(addLessLoader());
+module.exports = override(
+    addWebpackAlias({
+        ['@']: path.resolve(__dirname, 'src'),
+    }),
+    addLessLoader()
+);
