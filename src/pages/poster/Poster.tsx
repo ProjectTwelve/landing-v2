fbc481ce3a530c935e53ff928d3ed3936e0beea4
秦书羽 <qinshuyu@bytedance.com>
2022-04-11 21:23:00 +0800

    原画效果尝试

--- src/pages/poster/Poster.tsx
+++ src/pages/poster/Poster.tsx
@@ -1,44 +1,76 @@
 import { useSize } from 'ahooks';
 import React, { useEffect, useMemo, useRef, useState } from 'react';
+import Parallax from 'parallax-js';
 import './Poster.less';
 
 export const Poster: React.FC = () => {
     const containerRef = useRef<HTMLDivElement>(null);
+    const bgRef = useRef<HTMLDivElement>(null);
     const containerSize = useSize(containerRef);
 
     // 根据原画宽高比计算出 cover 的宽高
-    const coverBgSize = useMemo(() => {
-        const originWidth = 4196;
-        const originHeight = 2160;
-        const ratio = originWidth / originHeight;
-        if (!containerSize) {
-            return { width: 0, height: 0 };
-        } else {
+    useEffect(() => {
+        if (!bgRef.current) {
+            return;
+        }
+        let left = 0;
+        let top = 0;
+        let width = 0;
+        let height = 0;
+        const ratio = 4196 / 2160;
+        if (containerSize) {
             if (containerSize.width / containerSize.height >= ratio) {
-                return {
-                    width: containerSize.width,
-                    height: containerSize.width / ratio,
-                };
+                left = 0;
+                top = (containerSize.width / ratio - containerSize.height) / -2;
+                width = containerSize.width;
+                height = containerSize.width / ratio;
             } else {
-                return {
-                    width: ratio * containerSize.height,
-                    height: containerSize.height,
-                };
+                left =
+                    (ratio * containerSize.height - containerSize.width) / -2;
+                top = 0;
+                width = ratio * containerSize.height;
+                height = containerSize.height;
             }
         }
+        bgRef.current.style.left = `${left}px`;
+        bgRef.current.style.top = `${top}px`;
+        bgRef.current.style.width = `${width}px`;
+        bgRef.current.style.height = `${height}px`;
     }, [containerSize]);
+
+    useEffect(() => {
+        if (!bgRef.current) {
+            return;
+        }
+        const parallaxInstance = new Parallax(bgRef.current, {});
+        return () => {
+            parallaxInstance.destroy();
+        };
+    }, []);
 
     return (
         <div className='poster' ref={containerRef}>
-            <div
-                className='poster__bg'
-                style={{ width: coverBgSize.width, height: coverBgSize.height }}
-            >
-                <div className='poster__img poster__img--1'></div>
-                <div className='poster__img poster__img--2'></div>
-                <div className='poster__img poster__img--3'></div>
-                <div className='poster__img poster__img--4'></div>
-                <div className='poster__img poster__img--5'></div>
+            <div className='poster__bg' ref={bgRef}>
+                <div
+                    data-depth-x='0'
+                    className='poster__img poster__img--1'
+                ></div>
+                <div
+                    data-depth-x='-0.15'
+                    className='poster__img poster__img--2'
+                ></div>
+                <div
+                    data-depth-x='0.1'
+                    className='poster__img poster__img--3'
+                ></div>
+                <div
+                    data-depth='-0.2'
+                    className='poster__img poster__img--4'
+                ></div>
+                <div
+                    data-depth='0.3'
+                    className='poster__img poster__img--5'
+                ></div>
             </div>
         </div>
     );
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,3 +3,5 @@ declare module '*.module.less' {
     const classes: { [key: string]: string };
     export default classes;
 }
+
+declare module 'parallax-js';
