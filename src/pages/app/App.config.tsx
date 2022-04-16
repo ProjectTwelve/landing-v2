a87784980b3765dadaa129d7be842c2f31cb9c7f
秦书羽 <qinshuyu@bytedance.com>
2022-04-16 17:01:37 +0800

    完善整体结构

---
+++ src/pages/app/App.config.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Home } from '../home/Home';
+import { Avatar } from '../avatar/Avatar';
+import { Tree } from '../tree/Tree';
+import { Poster } from '../poster/Poster';
+import { About } from '../about/About';
+import { Loading } from '../loading/Loading';
+
+export enum PageType {
+    Loading,
+    Home,
+    Avatar,
+    Tree,
+    Poster,
+    About,
+}
+
+export const CONTENT_PAGES = [
+    {
+        type: PageType.Loading,
+        NavText: null,
+        Content: <Loading />,
+    },
+    {
+        type: PageType.Home,
+        NavText: null,
+        Content: <Home />,
+    },
+    {
+        type: PageType.Avatar,
+        NavText: <>GAME&nbsp;CHARACTER</>,
+        Content: <Avatar />,
+    },
+    {
+        type: PageType.Tree,
+        NavText: <>ECONOMIC&nbsp;SYSTEM</>,
+        Content: <Tree />,
+    },
+    {
+        type: PageType.Poster,
+        NavText: <>GAME&nbsp;SCENE</>,
+        Content: <Poster />,
+    },
+    {
+        type: PageType.About,
+        NavText: <>ABOUT&nbsp;US</>,
+        Content: <About />,
+    },
+    {
+        type: null,
+        NavText: <>COMMUNITY</>,
+        Content: null,
+    },
+];
--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,23 +1,43 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { AppBg } from './components/AppBg';
 import './App.less';
-import { Home } from '../home/Home';
-import { Avatar } from '../avatar/Avatar';
-import { Tree } from '../tree/Tree';
-import { Poster } from '../poster/Poster';
-import { About } from '../about/About';
+import classnames from 'classnames';
+import { PageType, CONTENT_PAGES } from './App.config';
 
 export const App = () => {
+    const [current, setCurrent] = useState(PageType.Loading);
+
     return (
         <div className='app'>
             <AppBg />
-            <div className='logo'></div>
             <div className='content'>
-                <Home />
-                <Avatar />
-                <Tree />
-                <Poster />
-                <About />
+                {CONTENT_PAGES.map((p, i) => {
+                    return <>{p.Content}</>;
+                })}
+            </div>
+            <div className='logo'></div>
+            <div className='nav'>
+                {CONTENT_PAGES.map((p, i) => {
+                    return (
+                        p.NavText && (
+                            <div
+                                key={`${p.type}-${i}`}
+                                className={classnames(
+                                    'nav__item',
+                                    current === p.type && 'active'
+                                )}
+                                onClick={() => p.type && setCurrent(p.type)}
+                            >
+                                {p.NavText}
+                            </div>
+                        )
+                    );
+                })}
+            </div>
+            <div className='coming-btn'></div>
+            <div className='footer'>
+                <div className='footer__info'></div>
+                <div className='footer__audio audio-btn'></div>
             </div>
         </div>
     );
