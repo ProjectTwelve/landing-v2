6193a765318d4387866a494273a08d1f195d637b
秦书羽 <qinshuyu@bytedance.com>
2022-04-30 10:34:45 +0800

    完善

--- src/pages/loading/Loading.tsx
+++ src/pages/loading/Loading.tsx
@@ -43,7 +43,8 @@ export const Loading: React.FC = () => {
                 <a
                     className='loading__icon-link'
                     target='_blank'
-                    href='https://t.me/project_twelve' rel="noreferrer"
+                    href='https://t.me/project_twelve'
+                    rel='noreferrer'
                 >
                     <svg
                         className='telegram-icon'
@@ -60,7 +61,7 @@ export const Loading: React.FC = () => {
                 <a
                     className='loading__icon-link'
                     target='_blank'
-                    href='https://twitter.com/_p12_' rel="noreferrer"
+                    href='https://twitter.com/_p12_'
                 >
                     <svg
                         className='twitter-icon'
@@ -73,7 +74,8 @@ export const Loading: React.FC = () => {
                 <a
                     className='loading__icon-link'
                     target='_blank'
-                    href='https://discord.gg/EMrbsZPbxs' rel="noreferrer"
+                    href='https://discord.gg/EMrbsZPbxs'
+                    rel='noreferrer'
                 >
                     <svg
                         className='discord-icon'
