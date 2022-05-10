e102fee635a38f8e9b7a22f35e0f929dea53e350
秦书羽 <qinshuyu@bytedance.com>
2022-05-10 20:03:49 +0800

    添加模型

--- src/pages/avatar/components/utils/AvatarGLItemCartoon.tsx
+++ src/pages/avatar/components/utils/AvatarGLItemCartoon.tsx
@@ -2,8 +2,12 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { getPublicAssetPath } from '../../../../utils';
 import { loadingEE, LoadingSourceType } from '../../../app/App.utils';
-import { AvatarGLItemBase } from './base/AvatarGLItemBase';
-export class AvatarGLItemCartoon extends AvatarGLItemBase {
+import { AvatarGLItemBaseWithParticle } from './base/AvatarGLItemBaseWithParticle';
+export class AvatarGLItemCartoon extends AvatarGLItemBaseWithParticle {
+    public particleCanvasWidth = 1080;
+    public particleCanvasHeight = 1080;
+    public particleImgOffset = 140;
+
     public extraNode = (
         <>
             <div className='avatar-extra-subtitle'>Server DevOps</div>
@@ -16,7 +20,10 @@ export class AvatarGLItemCartoon extends AvatarGLItemBase {
         if (this.loadingPromise) {
             return this.loadingPromise;
         }
+
         this.loadingPromise = new Promise((resolve, reject) => {
+            let gltfLoaded = false;
+            let imageLoaded = false;
             new GLTFLoader().load(
                 getPublicAssetPath('files/avatar/avatar-cartoon.glb?v051001'),
                 (gltf) => {
@@ -26,9 +33,15 @@ export class AvatarGLItemCartoon extends AvatarGLItemBase {
                     this.scene.add(model);
                     this.mixer = new THREE.AnimationMixer(model);
                     // this.mixer?.clipAction(gltf.animations?.[0])?.play();
-                    this.loaded = true;
+                    gltfLoaded = true;
+                    this.loaded = gltfLoaded && imageLoaded;
                     this.render();
-                    resolve();
+                    if (this.loaded) {
+                        resolve();
+                    }
+                    this.scene.add(
+                        new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6)
+                    );
                     loadingEE.emit(
                         `progress.${LoadingSourceType.AVATAR_GLTF_CARTOON}`,
                         1
@@ -38,6 +51,28 @@ export class AvatarGLItemCartoon extends AvatarGLItemBase {
                     loadingEE.emit(
                         `progress.${LoadingSourceType.AVATAR_GLTF_CARTOON}`,
                         event.total ? (event.loaded / event.total) * 0.95 : 0.5
+                    );
+                }
+            );
+
+            const imageUrls = new Array(360).fill(0).map((_, i) => {
+                return getPublicAssetPath(
+                    `files/avatar/avatar-cartoon-particle/${i + 1 + 60000}.jpg`
+                );
+            });
+            const imageLoader = new THREE.ImageLoader();
+            Promise.all(imageUrls.map((url) => imageLoader.load(url))).then(
+                (data) => {
+                    this.imageDataArray = data;
+                    imageLoaded = true;
+                    this.loaded = gltfLoaded && imageLoaded;
+                    this.render();
+                    if (this.loaded) {
+                        resolve();
+                    }
+                    loadingEE.emit(
+                        `progress.${LoadingSourceType.AVATAR_GLTF_CARTOON_PARTICLE}`,
+                        1
                     );
                 }
             );
