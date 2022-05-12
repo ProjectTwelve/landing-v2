b2882b9e661fcac70811e91952658da28cc5b230
秦书羽 <qinshuyu@bytedance.com>
2022-05-12 13:59:20 +0800

    完善 wallgl 组件

--- src/pages/wall/components/WallGL.tsx
+++ src/pages/wall/components/WallGL.tsx
@@ -6,15 +6,19 @@ import './WallGL.less';
 
 export const WallGL = (props) => {
     const containerRef = useRef<HTMLDivElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
     usePageVisible(PageType.Wall, () => {
-        if (!containerRef.current) {
+        const canvas = canvasRef.current;
+        const container = containerRef.current;
+        if (!canvas || !container) {
             return;
         }
+        let frameId: number;
+        let timeoutId: number;
+        let intervalId: number;
         let gui = null;
         const hpgButterfly = (window as any).hpgButterfly;
-        const canvas = document.querySelector('canvas#butterfly');
-        const wall = document.querySelector('.wall');
         // for render timedelta calc
         let time;
         // butterfly properties obj ref
@@ -22,93 +26,125 @@ export const WallGL = (props) => {
 
         let isMultiTouch = false;
         let multiTouchChargeToggle = false;
-        let touchEvents : any[] = [];
+        let touchEvents: any[] = [];
         let inputXY;
 
         const onDown = (evt) => {
-            inputXY = butterflyHelpers.getInputXY(evt, wall);
+            inputXY = butterflyHelpers.getInputXY(evt, container);
             onMove(evt);
         };
-        const onUp = (evt) => {
-        };
+        const onUp = (evt) => {};
         const onMove = (evt) => {
-            var newInputXY = butterflyHelpers.getInputXY(evt, wall);
+            var newInputXY = butterflyHelpers.getInputXY(evt, container);
             hpgButterfly.properties.mouse.set(newInputXY.x, newInputXY.y); // x, y as in {x:  -1 to 1, y: -1 to 1 }
             if (!isMultiTouch) {
                 if (!inputXY) inputXY = newInputXY;
-                hpgButterfly.draw(inputXY.x, inputXY.y, newInputXY.x, newInputXY.y);
+                hpgButterfly.draw(
+                    inputXY.x,
+                    inputXY.y,
+                    newInputXY.x,
+                    newInputXY.y
+                );
             }
             inputXY = newInputXY;
-        }
+        };
         const onClick = (evt) => {
             // to restart and regenerate the butterfly
             hpgButterfly.reset();
-        }
+        };
         const onResize = () => {
             // resize the web canvas to the screen size
             hpgButterfly.resize(window.innerWidth, window.innerHeight);
-        }
+        };
         const loop = () => {
-            requestAnimationFrame(loop);
-            [time, multiTouchChargeToggle] = butterflyHelpers.render(hpgButterfly, time, isMultiTouch, touchEvents, multiTouchChargeToggle);
-        }
+            // 性能优化：只在 wall 显示时调用
+            // 用于测试是否渲染
+            // console.log('wall loop');
+            frameId = requestAnimationFrame(loop);
+            [time, multiTouchChargeToggle] = butterflyHelpers.render(
+                hpgButterfly,
+                time,
+                isMultiTouch,
+                touchEvents,
+                multiTouchChargeToggle
+            );
+        };
         const init = () => {
+            if (!hpgButterfly.checkIsSupported(canvas)) {
+                return;
+            }
             hpgButterfly.init();
-            hpgButterfly.reset();
             time = +new Date() / 1000;
-
+            // you can alter the input event as you wish to or even use some input event library.
+            container!.addEventListener('mousedown', onDown);
+            container!.addEventListener(
+                'touchstart',
+                butterflyHelpers.getTouchBound(onDown, touchEvents, container)
+            );
+            container!.addEventListener('mousemove', onMove);
+            container!.addEventListener(
+                'touchmove',
+                butterflyHelpers.getTouchBound(onMove, touchEvents, container)
+            );
+            container!.addEventListener('mouseup', onUp);
+            container!.addEventListener(
+                'touchend',
+                butterflyHelpers.getTouchBound(onUp, touchEvents, container)
+            );
+            container!.addEventListener('click', onClick);
             window.addEventListener('resize', onResize);
             onResize();
-            loop();
-
-            // you can alter the input event as you wish to or even use some input event library.
-            wall!.addEventListener('mousedown', onDown);
-            wall!.addEventListener('touchstart', butterflyHelpers.getTouchBound(onDown, touchEvents, wall));
-            wall!.addEventListener('mousemove', onMove);
-            wall!.addEventListener('touchmove', butterflyHelpers.getTouchBound(onMove, touchEvents, wall));
-            wall!.addEventListener('mouseup', onUp);
-            wall!.addEventListener('touchend', butterflyHelpers.getTouchBound(onUp, touchEvents, wall));
-            wall!.addEventListener('click', onClick);
 
             properties = hpgButterfly.properties;
-            const butterflyIdx = properties.scene.children.findIndex(elem => elem.type === 'Object3D');
+            const butterflyIdx = properties.scene.children.findIndex(
+                (elem) => elem.type === 'Object3D'
+            );
             properties.scene.children.splice(butterflyIdx);
             for (let key in butterflyHelpers.defaultProperties) {
                 properties[key] = butterflyHelpers.defaultProperties[key];
             }
-
             // for param tweaking
             // gui = butterflyHelpers.turnOnGui();
-            
-            setTimeout(() => {
-                setInterval(
+        };
+        const onVisible = () => {
+            onResize();
+            time = +new Date() / 1000;
+            hpgButterfly.reset();
+            loop();
+            timeoutId = window.setTimeout(() => {
+                intervalId = window.setInterval(
                     butterflyHelpers.startParamDrift.bind(
                         null,
                         butterflyHelpers.defaultProperties,
                         properties
-                    ), 6000);
+                    ),
+                    6000
+                );
             }, 3000);
-        }
+        };
+        const onHide = () => {
+            clearTimeout(timeoutId);
+            clearInterval(intervalId);
+            cancelAnimationFrame(frameId);
+        };
+
+        init();
         return {
             onVisible: () => {
-                if (hpgButterfly.checkIsSupported (canvas)) {
-                    init();
-                } else {
-                    console.warn('No WEBGL. Aborting..');
-                }
+                onVisible();
             },
             onHide: () => {
-                // 界面隐藏的逻辑，清除负作用
+                onHide();
             },
             onDestroy: () => {
-                // 组件 unMount
+                window.removeEventListener('resize', onResize);
             },
         };
     });
 
     return (
         <div className='wall-gl' ref={containerRef}>
-            <canvas id='butterfly'></canvas>
+            <canvas className='butterfly-canvas' ref={canvasRef}></canvas>
         </div>
     );
 };
