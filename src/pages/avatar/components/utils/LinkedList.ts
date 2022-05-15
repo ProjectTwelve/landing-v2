476aae95d7ee8d3fee822891643ce65fb733894b
rexjz <robbort.xu@gmail.com>
2022-05-15 18:45:40 +0800

    chore: 链表

---
+++ src/pages/avatar/components/utils/LinkedList.ts
@@ -0,0 +1,33 @@
+class Node<T> {
+    public next: Node<T> | null = null;
+    public value: T | null = null;
+    constructor (value: T | null) {
+        this.value = value;
+    }
+}
+
+export default class LinkedList<T> {
+    sentry: Node<T> = new Node<T>(null);
+    tail: Node<T> | null = null;
+
+    constructor() {
+        this.sentry = new Node<T>(null);
+        this.tail = this.sentry; 
+    }
+
+    add(value: T) {
+        this.tail!.next = new Node(value);
+        this.tail!.next = this.tail;
+    }
+
+    forEach(interator: (node: Node<T>, prevNode: Node<T>, index: number) => void) {
+        let currentNode = this.sentry.next!
+        let prevNode = this.sentry;
+        let index = 0;
+        for(;currentNode.next != null;currentNode = currentNode.next!) {
+            interator(currentNode, prevNode, index);
+            prevNode = currentNode;
+        }
+        index += 1;
+    }
+}
