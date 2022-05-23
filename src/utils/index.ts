5a434762cb7e1cee00923c91f4cc7becf425e1d3
秦书羽 <qinshuyu@bytedance.com>
2022-05-23 22:48:38 +0800

    add tips

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -42,6 +42,30 @@ export const IS_MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Ope
     navigator.userAgent
 );
 
+// 移动端请求陀螺仪权限
+let hasRequested = false;
+export function requestOrientationPermission() {
+    return new Promise<void>((resolve) => {
+        if (
+            !hasRequested &&
+            window.DeviceOrientationEvent &&
+            (window.DeviceOrientationEvent as any).requestPermission
+        ) {
+            (window.DeviceOrientationEvent as any)
+                .requestPermission()
+                .then((r) => {
+                    hasRequested = true;
+                    // 在这里调用陀螺仪事件
+                    // 某些设备，授权后，刷新页面才能生效
+                    console.log('DeviceOrientationEvent.requestPermission', r);
+                    resolve();
+                });
+        } else {
+            resolve();
+        }
+    });
+}
+
 export const resizeBodyRotation = throttle(() => {
     if (window.innerWidth > window.innerHeight) {
         document.body.classList.add('body-landscape');
@@ -66,50 +90,49 @@ export function addResizeHandle(fn: Function) {
 /** GA **/
 
 const GAtiming = {
-    startTime : -1,
-    endTime : -1,
+    startTime: -1,
+    endTime: -1,
     prevView: '',
-    calcTiming: function(webview: string) {
+    calcTiming: function (webview: string) {
         let diff = 0;
-        if (this.startTime === -1) { // begin timing
-            this.startTime = new Date().getTime(); 
-        }
-        else if (this.prevView !== webview) { // changed webview
-            this.endTime = new Date().getTime(); 
+        if (this.startTime === -1) {
+            // begin timing
+            this.startTime = new Date().getTime();
+        } else if (this.prevView !== webview) {
+            // changed webview
+            this.endTime = new Date().getTime();
 
-            diff = this.endTime -  this.startTime;
-            GAevent("timing", this.prevView.split('-')[0] + '-timing', diff);
-            
+            diff = this.endTime - this.startTime;
+            GAevent('timing', this.prevView.split('-')[0] + '-timing', diff);
+
             this.startTime = this.endTime;
         }
         this.prevView = webview;
-    }
-}
+    },
+};
 
 export const initGA = () => {
-    const GA_ID : string = (process.env.REACT_APP_GOOGLE_ANALYTICS_ID as string);
+    const GA_ID: string = process.env.REACT_APP_GOOGLE_ANALYTICS_ID as string;
     // initialize ga
     ReactGA.initialize(GA_ID);
-}
+};
 
-export const GAevent = (category: string, action:string, value=-1) => {
+export const GAevent = (category: string, action: string, value = -1) => {
     // event tracking
     if (value === -1) {
         ReactGA.event({
             category: category,
-            action: action
+            action: action,
         });
-    }
-    else {
+    } else {
         ReactGA.event({
             category: category,
             action: action,
-            value: value
+            value: value,
         });
     }
 
-    if (category === "webview") {
+    if (category === 'webview') {
         GAtiming.calcTiming(action);
     }
-    
-}
+};
