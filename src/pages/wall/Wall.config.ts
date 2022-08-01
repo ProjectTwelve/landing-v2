d31f4129c0d630f5944ee5dcb6e5ace229cf6407
ShallowGreen <183339728@qq.com>
2022-08-01 14:14:56 +0800

    feat: add featured on

---
+++ src/pages/wall/Wall.config.ts
@@ -0,0 +1,30 @@
+export const FEATURED_ON_DATA = [
+    {
+        name: 'Yahoo! Finance',
+        url: 'https://finance.yahoo.com/news/project-twelve-closes-8-million-124000354.html',
+    },
+    {
+        name: 'MarketWatch',
+        url: 'https://www.marketwatch.com/press-release/p12-secures-8m-to-build-sustainable-web3-gaming-amid-ongoing-genesis-soul-bound-nft-airdrop-2022-06-28?mod=search_headline',
+    },
+    {
+        name: 'Finanzen.net',
+        url: 'http://www.finanzen.net/nachricht/aktien/project-twelve-closes-8-million-funding-round-to-build-sustainable-web3-gaming-ecosystem-launches-genesis-nft-airdrop-11483912',
+    },
+    {
+        name: 'Seeking Alpha',
+        url: 'https://seekingalpha.com/pr/18848162-project-twelve-closes-8-million-funding-round-to-build-sustainable-web3-gaming-ecosystem-and',
+    },
+    {
+        name: 'Benzinga',
+        url: 'http://www.benzinga.com/pressreleases/22/06/n27880417/project-twelve-closes-8-million-funding-round-to-build-sustainable-web3-gaming-ecosystem-launches-',
+    },
+    {
+        name: 'Morningstar',
+        url: 'https://www.morningstar.com/news/pr-newswire/20220628ph01728/project-twelve-closes-8-million-funding-round-to-build-sustainable-web3-gaming-ecosystem-launches-genesis-nft-airdrop',
+    },
+    {
+        name: 'JustGamers',
+        url: 'https://www.justgamers.net/gaming/project-twelve-raises-8-million-to-accelerate-web3-gaming-development/',
+    },
+]
