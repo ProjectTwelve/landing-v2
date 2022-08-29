e55eea7de43bdaf0aea09b752a4a91a851429fea
yusixian <3450414733@qq.com>
2022-08-29 15:11:35 +0800

    feat: add sentry

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const P12_API_PREFIX = process.env.P12_API_PREFIX || 'https://news-test.p12.games';
+
+export const SENTRY_DSN =
+    process.env.SENTRY_DSN || 'https://21e709d4697149bdada12707651ac95a@o1375249.ingest.sentry.io/6687523';
