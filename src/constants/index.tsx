d7b54eae42dfe3f06e711a94faaa0cd07a97ffef
yusixian <3450414733@qq.com>
2022-08-31 18:30:42 +0800

    fix: p12 avator

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,7 +1,7 @@
 export const P12_API_PREFIX = process.env.P12_API_PREFIX || 'https://news-test.p12.games';
 
 export const P12_AUTHOR_AVATOR = {
-    dongbo: 'https://airdrop.p12.games/favicon.ico',
+    dongbo: 'https://cdn1.p12.games/p12-news/ndpwwhzpSTAjHtnz',
 };
 
 export enum P12_NEW_TYPE_LABEL {
