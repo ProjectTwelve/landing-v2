603bca0d32923f9afa11851403b526603178aff0
yusixian <3450414733@qq.com>
2022-10-10 15:54:59 +0800

    fix: social point events

--- src/pages/social/Social.tsx
+++ src/pages/social/Social.tsx
@@ -13,6 +13,8 @@ import './Social.less';
 
 export const Social: React.FC = () => {
     const [newModalOpen, setOpen] = useState(false);
+    const [isVisible, setVisible] = useState(false);
+
     const [newInfo, setNewInfo] = useState<NewInfoType | null>(null);
 
     usePageLockScroll(newModalOpen); // 弹窗时阻止滚动
@@ -34,6 +36,7 @@ export const Social: React.FC = () => {
         return {
             onVisible: () => {
                 const tl = gsap.timeline();
+                setVisible(true);
                 tl.fromTo(
                     '.page-wrap-social',
                     {
@@ -48,6 +51,7 @@ export const Social: React.FC = () => {
                 return tl.then();
             },
             onHide: () => {
+                setVisible(false);
                 const tl = gsap.timeline();
                 tl.fromTo(
                     '.page-wrap-social',
@@ -69,66 +73,68 @@ export const Social: React.FC = () => {
     return (
         <div className="social">
             <ButterflyGL page={PageType.Social} />
-            <div
-                className="social__wrapper"
-                onWheel={(e) => {
-                    const social = e.currentTarget;
-                    if (social?.scrollTop !== 0) {
-                        e.stopPropagation();
-                        e.nativeEvent.stopImmediatePropagation();
-                    }
-                }}
-            >
-                <div className="social__community">
-                    <div className="social__title">{'community & links'}</div>
-                    <div className="social__community-container">
-                        <SocialItem
-                            className="discord"
-                            onClick={() => GAevent('event', 'Soc-discord')}
-                            href="https://discord.com/invite/EMrbsZPbxs"
-                            icon={require('../../assets/social/discord.png')}
-                            title="Discord"
-                        />
-                        <SocialItem
-                            className="twitter"
-                            onClick={() => GAevent('event', 'Soc-twi')}
-                            href="https://twitter.com/_p12_"
-                            icon={require('../../assets/social/twitter.png')}
-                            title="Twitter"
-                        />
-                        <SocialItem
-                            className="mirror"
-                            onClick={() => GAevent('event', 'Soc-tele')}
-                            href="https://mirror.xyz/p12.eth"
-                            icon={require('../../assets/social/mirror.png')}
-                            title="Mirror"
-                        />
-                        <SocialItem
-                            className="airdrop"
-                            href="https://airdrop.p12.games"
-                            icon={require('../../assets/social/airdrop.png')}
-                            title="Airdrop"
-                        />
-                        <SocialItem
-                            className="badge"
-                            href="https://galaxy.eco/P12/"
-                            icon={require('../../assets/social/badge.png')}
-                            title="Badge"
-                        />
-                        <SocialItem
-                            className="whitepaper"
-                            href="https://p12.dev/whitepaper"
-                            icon={<img src={require('../../assets/social/github.png')} alt="whitepaper" />}
-                            title="Whitepaper"
-                        />
+            {isVisible && (
+                <div
+                    className="social__wrapper"
+                    onWheel={(e) => {
+                        const social = e.currentTarget;
+                        if (social?.scrollTop !== 0) {
+                            e.stopPropagation();
+                            e.nativeEvent.stopImmediatePropagation();
+                        }
+                    }}
+                >
+                    <div className="social__community">
+                        <div className="social__title">{'community & links'}</div>
+                        <div className="social__community-container">
+                            <SocialItem
+                                className="discord"
+                                onClick={() => GAevent('event', 'Soc-discord')}
+                                href="https://discord.com/invite/EMrbsZPbxs"
+                                icon={require('../../assets/social/discord.png')}
+                                title="Discord"
+                            />
+                            <SocialItem
+                                className="twitter"
+                                onClick={() => GAevent('event', 'Soc-twi')}
+                                href="https://twitter.com/_p12_"
+                                icon={require('../../assets/social/twitter.png')}
+                                title="Twitter"
+                            />
+                            <SocialItem
+                                className="mirror"
+                                onClick={() => GAevent('event', 'Soc-tele')}
+                                href="https://mirror.xyz/p12.eth"
+                                icon={require('../../assets/social/mirror.png')}
+                                title="Mirror"
+                            />
+                            <SocialItem
+                                className="airdrop"
+                                href="https://airdrop.p12.games"
+                                icon={require('../../assets/social/airdrop.png')}
+                                title="Airdrop"
+                            />
+                            <SocialItem
+                                className="badge"
+                                href="https://galaxy.eco/P12/"
+                                icon={require('../../assets/social/badge.png')}
+                                title="Badge"
+                            />
+                            <SocialItem
+                                className="whitepaper"
+                                href="https://p12.dev/whitepaper"
+                                icon={<img src={require('../../assets/social/github.png')} alt="whitepaper" />}
+                                title="Whitepaper"
+                            />
+                        </div>
+                    </div>
+                    <div className="social__news">
+                        <div className="social__title">latest</div>
+                        <NewList onItemClick={openNewInfoModal} />
+                        <NewInfoDialog newInfo={newInfo} open={newModalOpen} onClose={closeNewInfoModal} />
                     </div>
                 </div>
-                <div className="social__news">
-                    <div className="social__title">latest</div>
-                    <NewList onItemClick={openNewInfoModal} />
-                    <NewInfoDialog newInfo={newInfo} open={newModalOpen} onClose={closeNewInfoModal} />
-                </div>
-            </div>
+            )}
         </div>
     );
 };
