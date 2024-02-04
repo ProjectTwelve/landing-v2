e41e3355ebd37de97bb1ce0ced10485369187b10
yusixian <cosine_yu@qq.com>
2024-02-04 16:03:34 +0800

    feat: swiper poster work freeMode

--- src/pages/poster/components/PosterArcanaWorks.tsx
+++ src/pages/poster/components/PosterArcanaWorks.tsx
@@ -1,15 +1,16 @@
+import classNames from 'classnames';
 import { useAtomValue } from 'jotai';
 import _ from 'lodash-es';
 import { useEffect, useMemo } from 'react';
 import Swiper from 'swiper';
 import 'swiper/css';
-import { Autoplay, EffectCoverflow } from 'swiper/modules';
+import { FreeMode } from 'swiper/modules';
 import { useIsPortrait } from '../../../hooks/useIsPortrait';
 import { premiumListAtom } from '../../../store/poster/state';
 import ArcanaGame from './ArcanaGame';
 import './PosterArcanaWorks.less';
-import classNames from 'classnames';
-Swiper.use([Autoplay, EffectCoverflow]);
+
+Swiper.use([FreeMode]);
 
 export default function PosterArcanaWorks() {
     const data = useAtomValue(premiumListAtom);
@@ -17,34 +18,26 @@ export default function PosterArcanaWorks() {
     const isPortrait = useIsPortrait();
     useEffect(() => {
         let arcanaSwiper = new Swiper('#arcana-game-swiper', {
-            effect: 'coverflow',
-            grabCursor: true,
-            init: false,
             centeredSlides: true,
             loop: true,
-            initialSlide: 2,
             slidesPerView: 'auto',
-            coverflowEffect: {
-                rotate: 0,
-                stretch: 3,
-                depth: 100,
-                modifier: 1,
-                scale: 0.9,
-                slideShadows: false,
-            },
+            freeMode: { enabled: true, minimumVelocity: 0.001, momentumBounceRatio: 1.2 },
+            spaceBetween: 20,
         });
-        arcanaSwiper?.init();
+
         return () => {
             arcanaSwiper?.destroy();
         };
-    }, [isPortrait]);
+    }, []);
+
     return (
         <div className="poster-arcana-works">
             <h1>Featured Creations</h1>
-            {/* {isLoading && <LoadingSvg color="white" className="poster-arcana-works__loading" />} */}
             <div
                 id="arcana-game-swiper"
-                className={classNames('poster-arcana-works__swiper swiper', { 'f-clip-hidden': !isPortrait })}
+                className={classNames('poster-arcana-works__swiper swiper', {
+                    'f-clip-hidden': !isPortrait,
+                })}
             >
                 <div className="swiper-wrapper">
                     {premiumItems?.length
@@ -52,10 +45,8 @@ export default function PosterArcanaWorks() {
                         : null}
                 </div>
             </div>
-            <div className={classNames('poster-arcana-works__swiper swiper', { 'f-clip-hidden': isPortrait })}>
-                {premiumItems?.length
-                    ? premiumItems.map((item) => <ArcanaGame className="swiper-slide" key={item.id} data={item} />)
-                    : null}
+            <div className={classNames('poster-arcana-works__swiper', { 'f-clip-hidden': isPortrait })}>
+                {premiumItems?.length ? premiumItems.map((item) => <ArcanaGame key={item.id} data={item} />) : null}
             </div>
         </div>
     );
--- src/pages/poster/components/PosterBg.tsx
+++ src/pages/poster/components/PosterBg.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import YouTube from 'react-youtube';
 import PosterDialog from '../../../components/dialog/PosterDialog';
 import Popover from '../../../components/popover';
@@ -9,27 +9,30 @@ import PosterSummary from './PosterSummary';
 
 export const PosterBg = ({ className }: { className?: string }) => {
     const vidRef = useRef<HTMLVideoElement>(null);
-
+    useEffect(() => {
+        vidRef?.current?.play();
+    }, []);
     return (
-        <div className={className}>
+        <>
             <video
                 ref={vidRef}
                 className="poster-mobile-bg"
                 src={getPublicAssetPath('files/vid/poster-bg.mp4')}
                 muted
                 loop
-                autoPlay
                 playsInline
             ></video>
-            <PosterDialog render={() => <YouTube className="poster__video" videoId={'wZ5KAc-M4Oc'} />}>
-                <div className="poster__point" style={{ left: '0.5rem', bottom: '1.5rem' }}></div>
-            </PosterDialog>
-            <Popover placement="top" render={({ close }) => <PosterSummary />} hoverOpen>
-                <div className="poster__point" style={{ left: '2.05rem', bottom: '2rem' }}></div>
-            </Popover>
-            <PosterDialog render={({ close }) => <PosterArcanaWorks />}>
-                <div className="poster__point" style={{ left: '3rem', bottom: '1rem' }}></div>
-            </PosterDialog>
-        </div>
+            <div className={className}>
+                <PosterDialog render={() => <YouTube className="poster__video" videoId={'wZ5KAc-M4Oc'} />}>
+                    <div className="poster__point" style={{ left: '0.5rem', bottom: '1.5rem' }}></div>
+                </PosterDialog>
+                <Popover placement="top" render={({ close }) => <PosterSummary />} hoverOpen>
+                    <div className="poster__point" style={{ left: '2.05rem', bottom: '1.5rem' }}></div>
+                </Popover>
+                <PosterDialog render={({ close }) => <PosterArcanaWorks />}>
+                    <div className="poster__point" style={{ left: '2.85rem', bottom: '3rem' }}></div>
+                </PosterDialog>
+            </div>
+        </>
     );
 };
